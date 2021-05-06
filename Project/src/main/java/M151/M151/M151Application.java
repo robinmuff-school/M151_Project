f99435b14b85aa7d71f903faa2bacb816549ad69
Robin <robin.muff@bluewin.ch>
2021-05-06 09:16:49 +0200

    Initial Project Setup

---
+++ Project/src/main/java/M151/M151/M151Application.java
@@ -0,0 +1,13 @@
+package M151.M151;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M151Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M151Application.class, args);
+	}
+
+}
---
+++ Project/src/test/java/M151/M151/M151ApplicationTests.java
@@ -0,0 +1,13 @@
+package M151.M151;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M151ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
